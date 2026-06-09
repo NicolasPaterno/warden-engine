007c26d45e23f2ae3b7782532e95ced342e6cc4c
NicolasPaterno <nicolasps.contato@gmail.com>
2026-06-09 10:36:48 -0300

    feat(service): add alert read service

--- internal/service/alert_service.go
+++ internal/service/alert_service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"context"
+
+	engine "github.com/NicolasPaterno/warden-engine"
+)
+
+type AlertService struct {
+	repo engine.AlertRepository
+}
+
+func NewAlertService(repo engine.AlertRepository) *AlertService {
+	return &AlertService{repo: repo}
+}
+
+func (s *AlertService) GetAll(ctx context.Context) ([]engine.Alert, error) {
+	return s.repo.GetAll(ctx)
+}
+func (s *AlertService) GetByRoom(ctx context.Context, room string) ([]engine.Alert, error) {
+	return s.repo.GetByRoom(ctx, room)
+}
+func (s *AlertService) GetByRule(ctx context.Context, ruleID string) ([]engine.Alert, error) {
+	return s.repo.GetByRule(ctx, ruleID)
+}
